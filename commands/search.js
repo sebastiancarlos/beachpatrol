1d05db0f7ddebb27ce7d00fcbb254848c235f308
Sebastian Carlos <sebastiancarlos@gmail.com>
2025-06-14 22:23:09 -0300

    Remove sample command "start". Mention on README is enough

--- commands/search.js
+++
@@ -1,6 +0,0 @@
-export default async (context, ...searchTerms) => {
-  const page = await context.newPage();
-  await page.goto(
-    `https://www.google.com/search?q=${encodeURIComponent(searchTerms.join(" "))}`,
-  );
-};
